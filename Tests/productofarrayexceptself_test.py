2b9e39bb55119167055a09f08fa17161d90572a5
ishanupadhyay <ishanupadhyay412@gmail.com>
2025-12-26 22:31:06 +0530

    Updated pipeline and folder structures

--- Tests/productofarrayexceptself_test.py
+++ Tests/productofarrayexceptself_test.py
@@ -10,7 +10,7 @@ import os
 # Add the current directory to Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from productofarrayexceptself import ProductArray
+from Arrays.productofarrayexceptself import ProductArray
 
 
 def test_product_array():
