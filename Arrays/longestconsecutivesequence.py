35e03207937d4f67a8405452f9207dd471222c61
ishan412 <i.upadhyay2@newcastle.ac.uk>
2025-12-26 14:14:50 +0530

    Longest Consecutive Sequence O(n) O(n)

--- Arrays/longestconsecutivesequence.py
+++ Arrays/longestconsecutivesequence.py
@@ -1,12 +1,8 @@
 nums = [100,4,200,1,3,2]
 
-hash_set = set()
+hash_set = set(nums)
 length = 1
 max_length = 0
-
-for i in nums:
-    hash_set.add(i)
-
 
 for i in hash_set:
     if i - 1 not in hash_set:
